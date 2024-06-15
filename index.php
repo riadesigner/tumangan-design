164f5e5eaba46b8bc4a7575c642ca92b9cb04140
riadesigner <evgenii.pogrebnyak@gmail.com>
2024-06-15 16:43:00 +1000

    ok

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 </head>
 <body>
 <div class="site-all">
+    <div class="site-exclude-footer">
     <div class="site-top-row"></div>
     <div class="site-main-row">    
         <div class="site-left-column">
@@ -106,9 +107,70 @@
                     <div class="frontpage-news-blog__item">3</div>
                 </div>    
 
+                <div class="frontpage-section-title"><a href="#">СОБЫТИЯ</a></div>
+                <div class="frontpage-events">
+                    
+                    <div class="frontpage-events-row">
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>СЕНТЯБРЬ  / 2024</span></div>
+                            <h2>Открытие международного, речного круизного маршрута в Российско-китайско- северокорейском трехграничье. Проекты и инициативы</h2>
+                            <div class="number">1.</div>
+                        </a>
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>ОКТЯБРЬ / 2024</span></div>
+                            <h2>Трансграничный туристический кластер «Туристско-рекреационный комплекс в Хасанском районе РФ с осью движения по реке Туманная (Туманган)»  и инвестиционная программа проекта
+                            </h2>
+                            <div class="number">2.</div>
+                        </a>
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>ИЮЛЬ / 2024</span></div>
+                            <h2>Общественная обьединение «Туманганский путь», Цели. Задачи. Сотрудничество
+                            <div class="number">3.</div>
+                        </h2>
+                    </a>                            
+                    </div>
+                
+                    <div class="frontpage-events-row">
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>ФЕВРАЛЬ  / 2025</span></div>
+                            <h2>Открытие международного, речного круизного маршрута в Российско-китайско- северокорейском трехграничье. Проекты и инициативы</h2>
+                            <div class="number">4.</div>
+                        </a>
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>МАРТ / 2025</span></div>
+                            <h2>Трансграничный туристический кластер «Туристско-рекреационный комплекс в Хасанском районе РФ с осью движения по реке Туманная (Туманган)»  и инвестиционная программа проекта
+                            </h2>
+                            <div class="number">5.</div>
+                        </a>
+                        <a href="#" class="frontpage-events__item">
+                            <div class="date"><span>ИЮЛЬ / 2025</span></div>
+                            <h2>Общественная обьединение «Туманганский путь», Цели. Задачи. Сотрудничество
+                            <div class="number">6.</div>
+                        </h2>
+                    </a>                            
+                    </div>                                         
+                </div>    
+                
+                <div class="frontpage-section-title"><a href="#">ОБСУЖДЕНИЕ</a></div>
+                <div class="frontpage-talk">
+                    12312312
+                </div>                
+
+                <div class="frontpage-section-title"><a href="#">ДОКУМЕНТАЦИЯ</a></div>
+                <div class="frontpage-docs">
+                    12312312
+                </div> 
+
+                <div class="frontpage-section-title"><a href="#">ПАРТНЕРЫ ПРОЕКТА</a></div>
+                <div class="frontpage-docs">
+                    12312312
+                </div>                 
+
             </div>
         </div>
     </div>
+    </div>
+    
     <div class="site-footer">
     3
     </div>
